ae74d0d1778b80a8ccf0818dcaabf40abec3a3c9
Nail <strana39@yandex.ru>
2022-08-05 02:05:34 +0500

    Решение задачи 8

---
+++ homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int number = new int();
+int Count1 = new int();
+Console.WriteLine("Посмотрим на четные числа от 1 до N?");
+Console.WriteLine("Введите число N: ");
+number = Convert.ToInt32(Console.ReadLine());
+Count1 = 1;
+Console.WriteLine("Вот чётные числа в диапазоне:");
+while (Count1 > 0 && Count1 <= number)
+{
+    if (Count1 % 2 == 0)
+    {
+        Console.Write($"{Count1}  ");
+    }
+    Count1 = Count1 + 1;
+}
