ffff0bb721cee8af7a3683ad045d909014ff358c
Nail <strana39@yandex.ru>
2022-08-08 18:43:54 +0500

    add TASK 14

--- task12/Program.cs
+++ task12/Program.cs
@@ -2,6 +2,25 @@
 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 34, 5 -> не кратно, остаток 4 
 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+ int number = firstNumber % secondNumber; 
+ Console.WriteLine(number);
+
+if (number > 0)
+{
+    Console.WriteLine($"Остаток от деления: {number}");
+}
+else
+{
+     Console.WriteLine($"Число {secondNumber} кратно числу {firstNumber}");
+}
+
 */
 
 int numberOne = new Random().Next(1, 100);
---
+++ task14/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+Console.WriteLine("Введите число: ");
+int addNumber = Convert.ToInt32(Console.ReadLine());
+
+int constanteOne = 7;
+int constanteTwo = 23;
+
+if (addNumber % constanteOne == 0 && addNumber % constanteTwo == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+ 
+    Console.WriteLine("Число не кратно 7 и 23");
+}
