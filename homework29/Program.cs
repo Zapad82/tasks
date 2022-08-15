a216881ef8185299064a91fcf82940fc6f2e34b1
Zapad82 <strana39@yandex.ru>
2022-08-16 02:05:07 +0500

    Add Homework 29

---
+++ homework29/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] arr = new int[8]; // создали массив из восьми элементов
+
+void RandomArray(int[] array)
+{
+for (int i = 0; i < arr.Length-3; i++)
+    {
+        //Создание объекта для генерации чисел
+        Random rnd = new Random();
+        //Получить случайное число (в диапазоне от 0 до 10)
+        int value = rnd.Next(0, 100);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+void RandomArray2(int[] array)
+{
+for (int i = 0; i < arr.Length-5; i++)
+    {
+        //Создание объекта для генерации чисел
+        Random rnd = new Random();
+        //Получить случайное число (в диапазоне от 0 до 10)
+        int value = rnd.Next(0, 100);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+RandomArray(arr); 
+Console.WriteLine();
+RandomArray2(arr);
+Console.WriteLine();
