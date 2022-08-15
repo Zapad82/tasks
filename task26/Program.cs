e0e3ba680a3e0bbccf299a48cb59080ba987965a
Zapad82 <strana39@yandex.ru>
2022-08-15 22:46:55 +0500

    Add Task 26

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 26.
+Напишите программу, которая принимает на вход число и выдаёт 
+количество цифр в числе. 456 -> 3 78 -> 2 89126 -> 5 
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+int numLength(int number)
+{
+    int numberLength = num.ToString().Length;
+    return numberLength;
+}
+Console.WriteLine($"{num} => {numLength(num)}");
