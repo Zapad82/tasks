dae33601dad32ad398efc8045d067c1a6087b1ba
Zapad82 <strana39@yandex.ru>
2022-08-16 00:29:23 +0500

    Add Task 30

--- task28/Program.cs
+++ task28/Program.cs
@@ -1,23 +1,6 @@
 ﻿/*
 Задача 28: Напишите программу, которая принимает на вход число N
 и выдаёт произведение чисел от 1 до N. 4 -> 24 5 -> 120 
-
-int[] arr = new int[8]; // создали массив из восьми элементов
-
-void RandomArray(int[] array)
-{
-for (int i = 0; i < arr.Length; i++)
-{
-//Создание объекта для генерации чисел
-Random rnd = new Random();
-//Получить случайное число (в диапазоне от 0 до 10)
-int value = rnd.Next(0, 2);
-arr[i] = value;
-Console.Write($"{arr[i]} ");
-}
-}
-
-RandomArray(arr); 
 */
 
 Console.Clear();
