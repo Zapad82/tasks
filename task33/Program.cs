11acf624c66a390124cc737a2a9171252dab0861
Zapad82 <strana39@yandex.ru>
2022-08-18 23:58:07 +0500

    Add Tasks 31, 32, 33, 35

---
+++ task33/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 33: Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(5, 0, 999);
+Console.WriteLine();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i] == num)
+        {
+            Console.WriteLine($"Да, число {num} есть в заданном массиве.");
+            break;
+        }
+ 
+        
+    else
+        {
+            Console.WriteLine($"Нет, число {num} отсутствует в заданном массиве.");
+            break;
+        }
+}
---
+++ task35/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 35: Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+int[] arr = new int[123]; // создали массив из восьми элементов
+
+void RandomArray(int[] array)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+//Создание объекта для генерации чисел
+Random rnd = new Random();
+//Получить случайное число 
+int value = rnd.Next(1000);
+arr[i] = value;
+Console.Write($"{arr[i]} ");
+}
+}
+
+void IsNumInArray(int[] array)
+{
+int numExist = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+if (arr[i] > 10 && arr[i] < 99)
+{
+numExist++;
+}
+}
+if (numExist > 0)
+{
+Console.WriteLine($"Количество чисел - {numExist}");
+}
+else
+{
+Console.WriteLine($"Чисел нет - {numExist}");
+}
+}
+
+RandomArray(arr);
+IsNumInArray(arr); 
