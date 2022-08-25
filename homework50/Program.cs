accefb137dca6a1a1e9cfc2a763a73fc3b6b3b80
Zapad82 <strana39@yandex.ru>
2022-08-26 02:25:06 +0500

    Add Homework 50

---
+++ homework50/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 50: Напишите программу, которая на вход
+принимает позиции элемента в двумерном массиве, и
+возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.Clear();
+Console.WriteLine("Введите номер строки: ");
+int positionRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int positionColumn = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0,20);
+Console.WriteLine("С помощью генератора случайных чисел, мы получили массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+int[,] GetArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            result[i,j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i=0;i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1);j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+if (positionRow > array.GetLength(0) || positionColumn > array.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {positionRow} строки и {positionColumn} столбца равно {array[positionRow-1,positionColumn-1]}");
+}
